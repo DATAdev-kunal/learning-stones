3e2d63402d007a0f5ee268406d339aeec057b6c6
DATAdev-kunal <kunal.datadev.os@gmail.com>
2023-10-22 00:18:40 +0530

    added airline reservation system java project

---
+++ Java_Projects/Airline-Reservation-System/src/main/java/com/mycompany/airline/reservation/system/AirlineReservationSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airline.reservation.system;
+
+/**
+ *
+ * @author kunal
+ */
+public class AirlineReservationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
