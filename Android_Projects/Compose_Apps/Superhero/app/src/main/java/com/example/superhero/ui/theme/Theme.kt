67faddd30f38edb92c7dd06702cb3aa0270b041c
giant-nova <kunal.datadev.os@gmail.com>
2023-11-13 19:40:50 +0530

    adding compose projects

--- Android_Projects/Compose_Apps/Superhero/app/src/main/java/com/example/superhero/ui/theme/Theme.kt
+++ Android_Projects/Compose_Apps/Superhero/app/src/main/java/com/example/superhero/ui/theme/Theme.kt
@@ -1,14 +1,24 @@
 package com.example.compose
 
-import android.text.DynamicLayout
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.shapes
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.internal.isLiveLiteralsEnabled
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.platform.WindowRecomposerPolicy
+import androidx.core.view.WindowCompat
 import com.example.superhero.ui.theme.Shapes
+import com.example.superhero.ui.theme.Typography
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -77,21 +87,32 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun SuperheroAppTheme(
-  useDarkTheme: Boolean = isSystemInDarkTheme(),
-  dynamicColor: Boolean = false ,
-  content: @Composable() () -> Unit
-) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
-
-  MaterialTheme(
-    colorScheme = colors,
-    content = content,
-      shapes = Shapes,
-      
-      
-  )
+        darkTheme: Boolean = isSystemInDarkTheme(),
+        dynamicColor: Boolean = false,
+        content: @Composable () -> Unit
+){
+    val colorScheme = when{
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    }
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+    
+    val view = LocalView.current
+    if(!view.isInEditMode){
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+    
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
 }
