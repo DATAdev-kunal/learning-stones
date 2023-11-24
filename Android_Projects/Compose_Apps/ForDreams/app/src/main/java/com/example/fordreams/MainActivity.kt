eeeb0efef86066d1671855891b761409c6ecfbde
giant-nova <kunal.datadev.os@gmail.com>
2023-11-25 00:42:09 +0530

    added app for dreams

--- Android_Projects/Compose_Apps/ForDreams/app/src/main/java/com/example/fordreams/MainActivity.kt
+++ Android_Projects/Compose_Apps/ForDreams/app/src/main/java/com/example/fordreams/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.fordreams
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.fordreams.ui.theme.ForDreamsTheme
+import androidx.compose.ui.unit.dp
+import com.example.compose.ForDreamsTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +30,7 @@ class MainActivity : ComponentActivity() {
 		setContent {
 			ForDreamsTheme {
 				Surface(
-					modifier = Modifier.fillMaxSize(),
-					color = MaterialTheme.colorScheme.background
+					modifier = Modifier.fillMaxSize()
 				) {
 					ForDreamsApp()
 				}
@@ -31,6 +43,31 @@ class MainActivity : ComponentActivity() {
 fun ForDreamsApp() {
 
 }
+@Composable
+fun DreamItems(
+		modifier: Modifier = Modifier
+){
+	Card(
+		modifier = modifier
+	) {
+		Column(
+			horizontalAlignment = Alignment.CenterHorizontally,
+			modifier =  Modifier
+				.fillMaxSize()
+			
+		) {
+			Text(
+				text = "Univ_name"
+			)
+			Text(
+				text = "Welcome",
+				modifier = modifier
+					.padding(dimensionResource(id = R.dimen.padding_medium))
+			)
+			Image(painter = painterResource(id = R.id.), contentDescription = )
+		}
+	}
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -38,4 +75,10 @@ fun GreetingPreview() {
 	ForDreamsTheme {
 		ForDreamsApp()
 	}
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CardPreview(){
+	DreamItems()
 }
