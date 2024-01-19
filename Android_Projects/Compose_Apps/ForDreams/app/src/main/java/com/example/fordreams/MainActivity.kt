294f82fbeb441d2c98029d8150b9e7dd8d832a6c
giant-nova <kunal.datadev.os@gmail.com>
2024-01-19 06:37:02 +0530

    adding more apps

--- Android_Projects/Compose_Apps/ForDreams/app/src/main/java/com/example/fordreams/MainActivity.kt
+++ Android_Projects/Compose_Apps/ForDreams/app/src/main/java/com/example/fordreams/MainActivity.kt
@@ -1,28 +1,35 @@
 package com.example.fordreams
 
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.ForDreamsTheme
+import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,35 +56,53 @@ fun DreamItems(
 ){
 	Card(
 		modifier = modifier
+			.background(color = Color(R.color.purple_500))
+			.wrapContentSize()
+			.fillMaxWidth(0.8f)
+			.padding(10.dp)
 	) {
 		Column(
 			horizontalAlignment = Alignment.CenterHorizontally,
 			modifier =  Modifier
-				.fillMaxSize()
 			
 		) {
 			Text(
 				text = "Univ_name"
 			)
-			Text(
-				text = "Welcome",
-				modifier = modifier
-					.padding(dimensionResource(id = R.dimen.padding_medium))
+			Row(
+				modifier = Modifier
+			) {
+				Text(
+					text = "Welcome",
+					modifier = modifier
+						.padding(dimensionResource(id = R.dimen.padding_medium))
+				)
+			}
+			Image(
+				painter = painterResource(R.drawable.images),
+				contentDescription = "univ image",
+				modifier = Modifier
+					.sizeIn(dimensionResource(id = R.dimen.image_size))
 			)
-			Image(painter = painterResource(id = R.id.), contentDescription = )
 		}
 	}
 }
+@Composable
+fun UniversityDetails(
+	@StringRes universityName: Int,
 
+){
+
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ForDreamsPreview() {
 	ForDreamsTheme {
 		ForDreamsApp()
 	}
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = false)
 @Composable
 fun CardPreview(){
 	DreamItems()
