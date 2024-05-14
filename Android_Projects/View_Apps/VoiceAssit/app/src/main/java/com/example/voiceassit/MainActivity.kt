4c341ce0e896cb96bd0d54f94d19ddfeb1334703
giant-nova <kunal.datadev.os@gmail.com>
2024-05-15 05:00:59 +0530

    added My assistant project

--- Android_Projects/View_Apps/VoiceAssit/app/src/main/java/com/example/voiceassit/MainActivity.kt
+++ Android_Projects/View_Apps/VoiceAssit/app/src/main/java/com/example/voiceassit/MainActivity.kt
@@ -9,6 +9,12 @@ import android.speech.SpeechRecognizer
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
+import org.w3c.dom.Text
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -21,11 +27,13 @@ class MainActivity : AppCompatActivity() {
         gTTSbutton.setOnClickListener{
             askSpeechInput()
         }
+//        mlResponseViaAPI()
     }
-
+    
+    
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
         val textBox = findViewById<TextView>(R.id.tv)
+        super.onActivityResult(requestCode, resultCode, data)
         if(requestCode==RQ_SPEECH_REC && resultCode== Activity.RESULT_OK){
             val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
             textBox.text = result?.get(0).toString()
@@ -40,6 +48,39 @@ class MainActivity : AppCompatActivity() {
             i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
             i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak Now !")
             startActivityForResult(i,RQ_SPEECH_REC)
+//            registerForActivityResult(RQ_SPEECH_REC)
         }
+    }
+    private fun mlResponseViaAPI(){
+        val url = "https://projectapi-yrtybbyihq-uc.a.run.app/"
+
+        val responseView = findViewById<TextView>(R.id.mlResponseTv)
+        
+        val stringRequest: StringRequest = object : StringRequest(
+            Method.POST, url,
+            Response.Listener<String?> { response ->
+                try {
+                    val jsonObject = JSONObject(response)
+                    val data = jsonObject.getString("speech")
+                    responseView.text = data.toString()
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener { error ->
+                Toast.makeText(
+                    this@MainActivity,
+                    error.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }) {
+            override fun getParams(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["speech"] = responseView.text.toString()
+                return params
+            }
+        }
+        val queue = Volley.newRequestQueue(this@MainActivity)
+        queue.add(stringRequest)
     }
 }
